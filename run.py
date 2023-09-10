8bcad93f30685ea0643cef12f08e33124b8c2386
Ahmadalhindi <>
2023-09-10 15:26:03 +0000

    create constant variables and function to print a board

--- run.py
+++ run.py
@@ -1,48 +1,27 @@
 
 # Constant variables
-
-
+size = 5
+num_ships = 4
 
 # Other variables
-
+player_board = [[" [ ] " for x in range(size)] for x in range(size)]
+computer_board = [[" [ ] " for x in range(size)] for x in range(size)]
 
 
 # Game logo and starting
-
-
+print("Welcome to the battleships game")
+print(f"Board size: {size}, number of ship: {num_ships}")
+print("_" * 30)
 
 # Function to print the board that established
-
-
-
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
 # Function to randomly Add ships in the boards
-
-
-
 # Create boards for each of participants
-
-
-
-
 # Function to validate the entered data by the player
-
-
-
 # Player guesses and turn to play
-
-
-
 # Computer turn to play
-
-
-
 # Check for scores for each
-
-
-
 # Check for the last winner
-
-
-
-# Hints:
-
+# Hints
