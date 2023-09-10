629b9d7ed5cfb47263a8d1a95af8e17e48c54b59
Ahmadalhindi <>
2023-09-10 13:42:01 +0000

    initiate the structures and test the first command on terminal

--- run.py
+++ run.py
@@ -1,3 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Constant variables
+
+
+
+# Other variables
+
+
+
+# Game logo and starting
+
+
+
+# Function to print the board that established
+
+
+
+# Function to randomly Add ships in the boards
+
+
+
+# Create boards for each of participants
+
+
+
+
+# Function to validate the entered data by the player
+
+
+
+# Player guesses and turn to play
+
+
+
+# Computer turn to play
+
+
+
+# Check for scores for each
+
+
+
+# Check for the last winner
+
+
+
+# Hints:
+
